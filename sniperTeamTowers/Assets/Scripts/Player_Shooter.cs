139c51b1e4095fe7a84d00b70408834e32b3e554
lip ables <phillipables1@gmail.com>
2019-01-10 06:23:43 -0500

    private camera on start get component camera and cursor lockmode locked then visable is false

--- sniperTeamTowers/Assets/Scripts/Player_Shooter.cs
+++ sniperTeamTowers/Assets/Scripts/Player_Shooter.cs
@@ -3,5 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player_Shooter : MonoBehaviour {
+    private Camera playerCam;
 
+    private void Start()
+    {
+        playerCam = GetComponent<Camera>();
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
 }
