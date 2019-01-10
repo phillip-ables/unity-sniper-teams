2afcd07075b720833d90d59f567b029eddd0745b
lip ables <phillipables1@gmail.com>
2019-01-10 06:01:59 -0500

    start get type of charachter controller, update get axis for hor and vert stored in delta x and z vars, new vector3 movement to house x and z, clamp magnitude of the it and speed, times delta time then transform direction movement equals movement, char cont move movement

--- sniperTeamTowers/Assets/Scripts/Player_Movement.cs
+++ sniperTeamTowers/Assets/Scripts/Player_Movement.cs
@@ -7,4 +7,20 @@ public class Player_Movement : MonoBehaviour {
 
     private CharacterController _charCont;
 
+    private void Start()
+    {
+        _charCont = FindObjectOfType<CharacterController>();
+    }
+
+    private void Update()
+    {
+        float deltaX = Input.GetAxis("Horizontal") * speed;
+        float deltaZ = Input.GetAxis("Vertical") * speed;
+        Vector3 movement = new Vector3(deltaX, 0, deltaZ);
+        movement = Vector3.ClampMagnitude(movement, speed);
+
+        movement *= Time.deltaTime;
+        movement = transform.TransformDirection(movement);
+        _charCont.Move(movement);
+    }
 }
