ec4c787b9eeec802744e6cf96ad3f8ee210852ee
lip ables <phillipables1@gmail.com>
2019-01-10 06:37:27 -0500

    update, if mouse button 0 down then create a vector 3 to center of the pixel width and height of the camera then ray for screen to point of that vector3, then type cast a raycasthit bar, if raycast to ray then hit transform gameobject

--- sniperTeamTowers/Assets/Scripts/Player_Shooter.cs
+++ sniperTeamTowers/Assets/Scripts/Player_Shooter.cs
@@ -12,4 +12,21 @@ public class Player_Shooter : MonoBehaviour {
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
     }
+
+    private void Update()
+    {
+        // left mouse button
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 point = new Vector3(playerCam.pixelWidth / playerCam.pixelHeight / 2, 0);
+
+            Ray ray = playerCam.ScreenPointToRay(point);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                GameObject hitObject = hit.transform.gameObject;
+                //launch a coroutine in response to a hit
+            }
+        }
+    }
 }
