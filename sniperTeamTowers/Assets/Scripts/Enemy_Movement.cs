8b8ff62fc02ba73e8e60f6b0fe60894bfe234fb9
lip ables <phillipables1@gmail.com>
2019-01-10 07:47:43 -0500

    public float speed and obstaclerange, private bool alive, start alive is true, update if alive then translate position 0,0,speed at time

--- sniperTeamTowers/Assets/Scripts/Enemy_Movement.cs
+++ sniperTeamTowers/Assets/Scripts/Enemy_Movement.cs
@@ -3,5 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy_Movement : MonoBehaviour {
-    
+    public float speed = 3.0f;
+    public float obstacleRange = 5.0f;
+
+    private bool _alive;
+
+    private void Start()
+    {
+        _alive = true;
+    }
+
+    private void Update()
+    {
+        if (_alive)
+        {
+            transform.Translate(0, 0, speed * Time.deltaTime);
+        }
+    }
 }
