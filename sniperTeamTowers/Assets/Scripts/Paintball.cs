270677580723ad7f76c3984cfa01678a42230c90
lip ables <phillipables1@gmail.com>
2019-01-11 02:52:46 -0500

    public float for speed and int for damage

--- sniperTeamTowers/Assets/Scripts/Paintball.cs
+++ sniperTeamTowers/Assets/Scripts/Paintball.cs
@@ -3,14 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Paintball : MonoBehaviour {
+    public float speed = 10.0f;
+    public int damage = 1;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
