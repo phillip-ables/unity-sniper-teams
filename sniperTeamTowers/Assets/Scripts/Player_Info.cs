a918dc4edc9f5a0fc16751a617e2a98ff71743ce
lip ables <phillipables1@gmail.com>
2019-01-11 02:46:36 -0500

    public void method for hurt that takes an int damage and minus player health with it then logs players health

--- sniperTeamTowers/Assets/Scripts/Player_Info.cs
+++ sniperTeamTowers/Assets/Scripts/Player_Info.cs
@@ -10,4 +10,10 @@ public class Player_Info : MonoBehaviour
     {
         _health = 10;
     }
+
+    public void Hurt(int damage)
+    {
+        _health -= damage;
+        Debug.Log("Health : " + _health);
+    }
 }
