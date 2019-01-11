65439b9469823ffe5988f6d4844273b1cd78b33b
lip ables <phillipables1@gmail.com>
2019-01-11 02:11:13 -0500

    public prefab game object, private enemy object, on update if enemy is null then instantiate prefab as gameobject then transform position to prigion, get a random angel between 0 and 360 then rotate object y axis to this random angel

--- sniperTeamTowers/Assets/Scripts/Enemy_Spawn.cs
+++ sniperTeamTowers/Assets/Scripts/Enemy_Spawn.cs
@@ -3,14 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy_Spawn : MonoBehaviour {
+    public GameObject enemyPrefab;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private GameObject enemy;
+
+    private void Update()
+    {
+        if(enemy == null)
+        {
+            enemy = Instantiate(enemyPrefab) as GameObject;
+            enemy.transform.position = new Vector3(0, 1, 0);
+
+            float angle = Random.Range(0, 360);
+            enemy.transform.Rotate(0, angle, 0);
+        }
+    }
 }
