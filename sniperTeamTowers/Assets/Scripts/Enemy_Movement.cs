f1e0cd526dcf64fde8242747ae30f9e998d6cdbf
lip ables <phillipables1@gmail.com>
2019-01-11 01:45:25 -0500

    on update new ray from position moving forward, instantiate raycast hit if spherecast from ray of a radius .75 has a raycast hit then check if the hit distance is in obstacle range if so get a random flaot from -110 110 and rotate the y transform

--- sniperTeamTowers/Assets/Scripts/Enemy_Movement.cs
+++ sniperTeamTowers/Assets/Scripts/Enemy_Movement.cs
@@ -19,5 +19,16 @@ public class Enemy_Movement : MonoBehaviour {
         {
             transform.Translate(0, 0, speed * Time.deltaTime);
         }
+
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+        if(Physics.SphereCast(ray, 0.75f, out hit))
+        {
+            if(hit.distance < obstacleRange)
+            {
+                float angle = Random.Range(-110, 110);
+                transform.Rotate(0, angle, 0);
+            }
+        }
     }
 }
