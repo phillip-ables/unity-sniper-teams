e3fd446ddd85821e8fe8d8432b52c80f1b3931ab
lip ables <phillipables1@gmail.com>
2019-01-11 02:53:19 -0500

    on update translate position 0, 0, speed at time

--- sniperTeamTowers/Assets/Scripts/Paintball.cs
+++ sniperTeamTowers/Assets/Scripts/Paintball.cs
@@ -6,4 +6,9 @@ public class Paintball : MonoBehaviour {
     public float speed = 10.0f;
     public int damage = 1;
 
+    private void Update()
+    {
+        transform.Translate(0, 0, speed * Time.deltaTime);
+    }
+
 }
