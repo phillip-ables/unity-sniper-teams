b8549f069d17c47082cad8a996834bea8e1fd7f6
lip ables <phillipables1@gmail.com>
2019-01-17 00:33:29 -0500

    public ints for bellets left and bullets per mag, public float for fire rate

--- sniperTeamTowers/Assets/Scripts/Weapon/Weapon.cs
+++ sniperTeamTowers/Assets/Scripts/Weapon/Weapon.cs
@@ -4,4 +4,9 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour {
 
+    public int bulletsPerMag = 30;
+    public int bulletsLeft;
+
+    public float fireRate = 0.1;  // 100 millisec
+
 }
